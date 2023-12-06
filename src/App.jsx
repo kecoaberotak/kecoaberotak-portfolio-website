282ab7987954125e8082331a13becf2a8f023802
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-06 13:20:47 +0700

    tambahin font dan warna

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
     <div className="relative">
       {/* Fixed */}
       <div className="relative md:fixed w-full md:w-7/12 min-h-screen inset-0">
-        <h1 className="absolute bottom-0 left-0 p-20 text-white text-8xl">Nero<br />The Kitten</h1>
+        <h1 className="absolute bottom-0 left-0 p-20 text-8xl text-biruMedium font-poppins">Nero<br />The Kitten</h1>
         <img src="kucing.jpg" className="object-cover w-full h-full" />
       </div>
       {/* Scroll */}
       <div className="w-full md:w-5/12 ml-auto">
         <div className="bg-red-200 h-screen flex justify-center items-center flex-col p-10">
-          <h2 className="text-4xl mb-5">Meet Benny</h2>
+          <h2 className="text-4xl mb-5">Meet Nero</h2>
           <p className="mb-5">I was born 20 May 2020</p>
         </div>
         <div className="bg-red-50 h-screen flex justify-center items-center flex-col p-10">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hitamDark: '#0E0F11',
+        hitamLight: '#1C1C1E',
+        biruDark: '#0A192F',
+        biruMedium: '#133954',
+        biruLight: '#7CD7C2',
+        abuLight: '#628290',
+        abuDark: '#A2A2A2',
+        putihDark: '#E4E4E4',
+        putihLight: '#FFFFFF'
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
+      }
+    },
   },
   plugins: [],
 }
