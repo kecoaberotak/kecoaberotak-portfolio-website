8f8778cf81721fb5645b4531586de857ee1f8074
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-06 18:58:59 +0700

    memisahkan section fixed ke component FixedComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import FixedSection from "./components/fragments/fixedSection"
+
 function App() {
 
   return (
     <div className="relative">
       {/* Fixed */}
-      <div className="relative md:fixed w-full md:w-7/12 min-h-screen inset-0">
-        <h1 className="absolute bottom-0 left-0 p-20 text-8xl text-biruMedium font-poppins">Nero<br />The Kitten</h1>
-        <img src="kucing.jpg" className="object-cover w-full h-full" />
-      </div>
+      <FixedSection></FixedSection>
       {/* Scroll */}
-      <div className="w-full md:w-5/12 ml-auto">
+      <section className="w-full md:w-5/12 ml-auto">
         <div className="bg-red-200 h-screen flex justify-center items-center flex-col p-10">
           <h2 className="text-4xl mb-5">Meet Nero</h2>
           <p className="mb-5">I was born 20 May 2020</p>
@@ -21,7 +20,7 @@ function App() {
           <h2 className="text-4xl mb-5">I love my humans</h2>
           <p className="mb-5">My humans are super special to me, and I love them</p>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
