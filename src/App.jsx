fcc568fc6929e169d5c440b24f01dded5cfa2434
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-06 19:03:54 +0700

    create ScrollSection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import FixedSection from "./components/fragments/fixedSection"
+import ScrollSection from "./components/fragments/ScrollSection"
 
 function App() {
 
   return (
     <div className="relative">
-      {/* Fixed */}
       <FixedSection/>
-      {/* Scroll */}
-      <section className="w-full md:w-5/12 ml-auto">
-        <div className="bg-red-200 h-screen flex justify-center items-center flex-col p-10">
-          <h2 className="text-4xl mb-5">Meet Nero</h2>
-          <p className="mb-5">I was born 20 May 2020</p>
-        </div>
-        <div className="bg-red-50 h-screen flex justify-center items-center flex-col p-10">
-          <h2 className="text-4xl mb-5">I love food</h2>
-          <p className="mb-5">Bones, Pallets, and more!</p>
-        </div>
-        <div className="bg-red-200 h-screen flex justify-center items-center flex-col p-10">
-          <h2 className="text-4xl mb-5">I love my humans</h2>
-          <p className="mb-5">My humans are super special to me, and I love them</p>
-        </div>
-      </section>
+      <ScrollSection/>
     </div>
   )
 }
---
+++ src/components/fragments/ScrollSection.jsx
@@ -0,0 +1,20 @@
+const ScrollSection = () => {
+  return(
+    <section className="w-full md:w-5/12 ml-auto">
+      <div className="bg-red-200 h-screen flex justify-center items-center flex-col p-10">
+        <h2 className="text-4xl mb-5">Meet Nero</h2>
+        <p className="mb-5">I was born 20 May 2020</p>
+      </div>
+      <div className="bg-red-50 h-screen flex justify-center items-center flex-col p-10">
+        <h2 className="text-4xl mb-5">I love food</h2>
+        <p className="mb-5">Bones, Pallets, and more!</p>
+      </div>
+      <div className="bg-red-200 h-screen flex justify-center items-center flex-col p-10">
+        <h2 className="text-4xl mb-5">I love my humans</h2>
+        <p className="mb-5">My humans are super special to me, and I love them</p>
+      </div>
+    </section>
+  );
+};
+
+export default ScrollSection;
