809ea00eaba016a3b561a6d285f6f0776b4ef306
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-07 13:32:05 +0700

    benerin hero

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return(
-    <div className="bg-biruDark h-screen flex justify-start items-center flex-col p-10">
-      <div className="w-[423px] h-[423px] rounded-full bg-transparent border-[5px] border-biruLight">
-        <img src="/dennis.png" alt="Dennis" className="object-contain"/>
+    <div className="bg-biruDark h-screen flex justify-center items-center flex-col p-10">
+      <div className="w-[423px] h-[423px] rounded-full bg-transparent border-[5px] border-biruLight overflow-hidden">
+        <img src="/dennis.png" alt="Dennis"/>
       </div>
     </div>
   );
