961f459d672615c97e47faff11a7ce40d90aa7e2
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-07 12:58:52 +0700

    edit project-card jadi link

--- src/components/elements/ProjectCard.jsx
+++ src/components/elements/ProjectCard.jsx
@@ -1,11 +1,13 @@
 const ProjectCard = () => {
   return (
     <div className="project-card">
-      <span className="text-putihLight font-semibold text-xs tracking-[0.3rem] mb-1">JAVASCRIPT</span>
-      <h3 className="font-semibold text-putihLight text-2xl mb-3">Dagingue</h3>
-      <p className="text-abuDark text-sm">Website company profile for beef slice shop. Build with 
-        <br />React.js, Node.js, MongoDB and Firebase
-      </p>
+      <a href="https://dagingue.vercel.app/" target="_blank" rel="noreferrer">
+        <span className="text-putihLight font-semibold text-xs tracking-[0.3rem] mb-1">JAVASCRIPT</span>
+        <h3 className="font-semibold text-putihLight text-2xl mb-3">Dagingue</h3>
+        <p className="text-abuDark text-sm">Website company profile for beef slice shop. Build with 
+          <br />React.js, Node.js, MongoDB and Firebase
+        </p>
+      </a>
     </div>
   );
 };
