1212fdbdadefb8e4fcae126e4f69da0a53a6aa05
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-08 14:32:16 +0700

    benerin ui hero

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return(
-    <div className="bg-biruDark h-screen justify-center items-center flex-col p-10 hidden sm:flex">
+    <div className="bg-biruDark h-screen justify-center items-center flex-col p-10 hidden lg:flex">
       <div className="w-[423px] h-[423px] rounded-full bg-transparent border-[5px] border-biruLight overflow-hidden">
         <img src="/dennis.png" alt="Dennis"/>
       </div>
