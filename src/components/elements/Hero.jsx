b4127137817e4fe7c697877784e8461795d847cb
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-12-08 17:38:10 +0700

    kelar edit ui

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return(
-    <div className="bg-biruDark h-screen justify-center items-center flex-col p-10 hidden lg:flex">
-      <div className="w-[423px] h-[423px] rounded-full bg-transparent border-[5px] border-biruLight overflow-hidden">
+    <div className="bg-biruDark h-screen justify-center items-center flex-col p-10 hidden md:flex">
+      <div className="w-[423px] h-[423px] rounded-full bg-transparent border-[5px] border-biruLight overflow-hidden scale-75 lg:scale-100">
         <img src="/dennis.png" alt="Dennis"/>
       </div>
     </div>
